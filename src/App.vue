5f4fa720f5dbfc4d239ebe981450c228a74bb9df
FuntSterving <wesbnk@gmail.com>
2023-08-28 20:48:50 +0600

    button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+const increas = () => {
+    counter.value++;
+}
 
+const decreas = () => {
+    counter.value--;
+}
 </script>
 
 <template>
-<div>Работает!</div>
+    <div class="test">
+        {{ counter }}
+        <button @click="increas">Увеличить</button>
+        <button @click="decreas">Уменьшить</button>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
