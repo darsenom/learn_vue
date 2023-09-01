bc107e63b2df75ff3481650f52e7ec9bd92c5e9e
FuntSterving <wesbnk@gmail.com>
2023-09-01 20:41:15 +0600

    card upd

--- src/components/CarItem.vue
+++ src/components/CarItem.vue
@@ -1,21 +1,62 @@
 <script setup>
-const car = defineProps({
-    car: {
-        type: Object,
-        required: true,
-    },
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+  car: {
+    type: Object,
+    required: true,
+  },
 })
+const carRemake = computed(() => {
+  return {
+    ...props.car,
+    price: props.car.price + '$',
+    kpd: props.car.year / props.car.volume,
+
+  }
+})
+
+function changeColor(color) {
+  const crappyColors = ['#FF0000', '#00FF00', '#0000FF'];
+  if (crappyColors.includes(color)) {
+    return true
+  }
+  return false
+} 
 </script>
 
 <template>
-    <div class="car">
-      <img :src="this.car.image" alt="car" />
-      <h2>{{ this.car.brand }}</h2>
-      <p>Цена: {{ this.car.price }}</p>
-      <p>Год выпуска: {{ this.car.year }}</p>
-      <p>Объем двигателя: {{this.car.volume }}</p>
-      <p>Цвет: {{ this.car.color }}</p>
+  <div class="car">
+    <img :src="carRemake.image" alt="car" />
+    <h2>{{ carRemake.brand }}</h2>
+    <p>Цена: {{ carRemake.price }}</p>
+    <p>Год выпуска: {{ carRemake.year }}</p>
+    <p>Объем двигателя: {{ carRemake.volume }}</p>
+    <p>Кпд: {{ carRemake.kpd }}</p>
+    <p :style="`color: ${carRemake.color}`">Цвет: {{ props.car.color }}</p>
+    <div class="chipes">
+      <span v-if="Number(carRemake.price.slice(0, -1)) > 1900000">Дорогой </span>
+      <span v-else-if="Number(carRemake.year) <= 1960">Старый</span>
+      <span v-else>Скучный</span>
+      <span v-if="changeColor(carRemake.color)">Конченный цвет</span>
+      <!-- <span v-show="carRemake.volume>2">Большой объем</span> -->
     </div>
-  </template>
 
-<style scoped></style>
+  </div>
+</template>
+
+<style scoped>
+.chipes{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+}
+.chipes span{
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid black;
+  border-radius:  5px;
+
+}
+</style>
--- src/components/CarList.vue
+++ src/components/CarList.vue
@@ -3,10 +3,10 @@ import CarItem from '@/components/CarItem.vue';
 const carsArray = [
     {
         brand: "BMW",
-        price: "2 000 000",
+        price: "2000000",
         year: 1994,
         volume: 2.4,
-        color: 'Red',
+        color: '#FF0000',
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFN0lN7T1WyAETO84V2umxeofkkkl6684bmI01rC_I&s"
     },
 ]
